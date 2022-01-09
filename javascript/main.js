774f66cd86f0b46df5b6f5004efbeb1f3de6619e
IsaLin777 <megan0928963902@gmail.com>
2022-01-09 16:02:18 +0800

    20220109 新增呼叫login語法，設定輸入帳號密碼條件。 新增迴圈語法while，設定總數後，產生亂數介於0~30之間。

--- javascript/main.js
+++ javascript/main.js
@@ -1,27 +1,30 @@
 // 瀏覽器的控制台 列印語法 console.log (顯示資料)
 /* 雙引號(")和單引號(')為字串 */
 
-TestCode ();
+// TestCode ();
 
-// 日期函數 Date 產生新的 Date
-var year = new Date ().getFullYear ();
-// var now = new Date ();
-// var year = now.getFullYear ();
+TimeYear ();
 
-// 年份變數
-// var Year = "2022"; 新增上方的日期函數，讓年份會隨著時間遞進自動更新年份，所以註記這行不會變動的定義2022
-document.getElementById('Year').textContent = year;
+function TimeYear (){
+    // 日期函數 Date 產生新的 Date
+    var year = new Date ().getFullYear ();
+    // var now = new Date ();
+    // var year = now.getFullYear ();
+
+    // 年份變數
+    // var Year = "2022"; 新增上方的日期函數，讓年份會隨著時間遞進自動更新年份，所以註記這行不會變動的定義2022
+    document.getElementById('Year').textContent = year;
+
+};
 
 // 設計 方法 function name (參數或空白){程式}
 function TestCode (){
     // 變數命名: 英文開頭，英文與數字，底線，javascript的關鍵字不能設定，英文可設定大小寫混用
-    // es5版本 var ，不可數字開頭
+    // es5版本 var ，(不可數字開頭);es6版本 let(變數)const(常數)
     // 宣告變數 全域變數
     var one;
     var ONE;
-    var One;
-    var O_ne;
-    var O_01ne;
+
 
     // 賦予 值
     console.log(one);
@@ -47,4 +50,103 @@ function TestCode (){
     else 
         console.log ("NumBer < Num 不成立");
 
+    // 多條件判斷式 if(條件){程式} else if (條件二) {程式}else{程式}
+    // 全域變數
+    let Num_One = 1;
+    let Num_Two = 2;
+    // ==等於 (比較)
+    if (Num_One == Num_Two) {
+        console.log ("兩個變數相等")
+    } else if (Num_Two > Num_One){
+        console.log (Num_Two + "大於" + Num_One);
+    } else {
+        console.log ("不成立");
+    };
+
+}
+
+// 全域變數
+var a = 1;
+let b = 2;
+
+// var 與 let 的差別
+function var_LetCode (){
+    // 區域變數
+    var a = 3;
+    let b = 4;
+
+    console.log ( a + b );
+}
+
+// Var_LetCode ();
+
+function var_LetCode(){
+
+    // var 與 let 的差別；暗藍色表示找不到變數，var的作用域會在function內才會區分[全域或區域]的差別；let本身的作用域包含了全部，例:function{...}if{...}等等
+    // 全域變數
+
+    var A = 4, C = 1 ;
+    let x = 5, y = 6 ;
+    {
+        var d = A + C ;
+        let z = x + y ;
+        // x和y的定義放在大括號之外，所以讀不到，讀z的定義時也就沒有x和y的定義
+    }
+
+    console.log(d);
+    // 呼叫不到z
+    console.log(z);
+
+    function NumCal(){
+        var d = A + C;
+        let z= x + y;
+    }
+
+}
+
+// 迴圈 for (宣告變數；條件；執行後增加或減少的次數){程式}
+let i;
+for ( i = 0; i <= 10; i = i + 1){
+    console.log (i);
+}
+
+// 迴圈 while (條件){程式}與 for 的差別，在於無法指定執行圈數
+function whileCode () {
+    // 總數
+    let Number = 200;
+    
+    // 亂數 (系統產生介於0~1之間)，將小數變為整數(數值要介於0~含30，需乘以31)，設定為無條件捨去法
+    let attack;
+    attack = Math.floor (Math.random() *30 );
+    // console.log (attack);
+}
+
+// login();
+
+function login(){
+    // 變數:承接使用者輸入的資料
+    // 帳號
+    let User;
+    // 密碼
+    let Number;
+
+    // 可輸入訊息的對話視窗語法
+    User = window.prompt ('請輸入帳號');
+
+    // 假設，網站有連接資料庫，帳號與密碼都由資料庫帶出
+    let UserName = 'shan';
+    let UserNum = '12345';
+
+    if (User == UserName){
+        
+        Number = window.prompt ('請輸入密碼');
+        
+        if (Number == UserNum){
+            console.log ("歡迎登入!");
+        } else {
+            console.log ("帳號或密碼有誤!");
+        }
+    } else {
+        console.log("帳號或密碼有誤!");
+    }
 }
