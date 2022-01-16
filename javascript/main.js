e4f0fb44b685bd7844ba6eed4e62f0d665a6d7aa
IsaLin777 <megan0928963902@gmail.com>
2022-01-16 17:34:19 +0800

    20220116 新增更多javascript語法:控Menu的打開與關閉。 控header的內容，從header.html叫過來。

--- javascript/main.js
+++ javascript/main.js
@@ -11,6 +11,7 @@ function TimeYear (){
 function CopyRightContent(){
     // 宣告變數 在html 內新增 div 標籤
     let div = document.createElement('div');
+    div.setAttribute('id',"Address");
 
     // 在哪個 html 標籤之內新增 div
     let Footer = document.getElementById('Footer');
@@ -30,7 +31,7 @@ function CopyRightContent(){
     // 調整 框架 位置
     // 1.在被選取的元素開頭插入內容prepend()置入被選取元素之中
     // 呼叫要選取的標籤
-    let.Address = document.getElementById('Address')
+    let Address = document.getElementById('Address');
     // Address.prepend(div);
 
     // 3.在被選取的元素之前插入內容 before()置入被選取元素標籤之前
@@ -46,13 +47,61 @@ function SmallNavCon(){
     let li = document.createElement('ul');
 
     // 在smallNav 內新增，宣告變數
-    let SN = document.getElementById('SmallNavOnly');
+    let SN = document.getElementById('SmallNav');
 
     // 建立ul框架，給予id名稱
     SN.appendChild(ul);
 
     // 賦予屬性id or class
     ul.setAttribute ("id", "PhoneNav");
+    let PhoneNav = document.getElementById('PhoneNav');
+
+    let Navclass = ['About','VerticalLine','Shop','VerticalLine','message','VerticalLine','Address'];
+    let NavCon = ['品牌故事','|','購物說明','|','客服留言','|','門市資訊'];
 
 
+    // 新增標籤時，程式的運作方式為:先新增標籤，賦予屬性，再指定新增的外框架(此為一個迴圈)
+    // 迴圈內的數字7，是因為陣列而產生，故要隨著陣列值的數量而自動增減
+
+    for (let i = 0; i < Navclass.length; i++){
+        let li = document.createElement('li');
+        // 新增屬性
+        li.setAttribute ("class", Navclass [i]);
+        // ul內新增li清單
+        PhoneNav.appendChild(li);
+
+        // 新增連結 a
+        let A = document.createElement('a');
+        // 新增屬性 假連結javascript:;
+        A.setAttribute ("href","javascript:;");
+        // li內新增a連結
+        li.appendChild(A);
+        // 置入 陣列內容
+        A.innerText = NavCon [i];
+    }
+
+
+}
+
+// MenuNav();
+// 行動裝置選單
+function MenuNav(){
+    let MenuNav = document.getElementById('MenuNav');
+    // 隱藏手機板選單
+    MenuNav.style.display = "none";
+}
+
+
+// PhoneMenuShow();
+// function PhoneMenuShow(){
+//     let Menu = document.getElementsByClassName('MenuBtn');
+//     // 顯示手機板選單內容
+//     Menu.style.display = "block";
+    
+// }
+
+
+Pageload();
+function Pageload(){
+    $('header').load("../web/header.html");
 }
